d1d1a07ac1d97c50b2d530c772d0246130b45eb7
shmadake <shmadake@gmail.com>
2026-07-04 20:00:01 +0530

    Initial commit: project setup with Spring Web, JPA, Security, Lombok, Postgres driver

---
+++ src/main/java/org/codevil/halflife/HalfLifeApplication.java
@@ -0,0 +1,13 @@
+package org.codevil.halflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalfLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codevil/halflife/HalfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codevil.halflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
